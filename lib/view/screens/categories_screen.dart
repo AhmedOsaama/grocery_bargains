653fb06bfc527e8155a167a5acaceb7f83eeef05
thedocf <firas.Selmen@esprit.tn>
2023-05-06 16:57:51 +0100

    Update categories_screen.dart

--- lib/view/screens/categories_screen.dart
+++ lib/view/screens/categories_screen.dart
@@ -107,6 +107,8 @@ class _CategoriesScreenState extends State<CategoriesScreen> {
             },
           ),
         );
+      } else {
+        chipsToShow = chips;
       }
     }
     super.initState();
